a69aec3f577a30701d5b09a8472586fcdd4c0fc3
Shahmil <64994036+shahmilc@users.noreply.github.com>
2023-06-10 17:37:19 -0400

    Add documentation

--- app.py
+++ app.py
@@ -102,8 +102,16 @@ def echo_cmd(user, channel, client, text):
 
     send_msg(client, msg)
 
-
+# Command to echo the reverse of the message
 def reverse_echo(user, channel, client, text):
+    """
+    Echo the command back in reverse
+    :param user: user id
+    :param channel: channel id
+    :param client: slack client
+    :param text: text of the message
+    :return: none
+    """
     msg = {
         "ts": "",
         "channel": channel,
@@ -122,20 +130,32 @@ def reverse_echo(user, channel, client, text):
 
     send_msg(client, msg)
 
-def remind_cmd(user, channel, client, text, bot_id):
+# Command to set a same-day reminder
+def remind_cmd(user, channel, client, text):
+    """
+    Set a same-day reminder
+    :param user: user id
+    :param channel: channel id
+    :param client: slack client
+    :param text: text of the message
+    :return: none
+    """
+    # Find the time in the message in the format HHMM
     parse_time = [x for x in text.split(" ") if x.isdigit()]
+
+    # Make sure the time code is appropriate
     if len(parse_time) > 0 and len(parse_time[0]) == 4 and int(parse_time[0]) < 2400:
+
         hour = int(parse_time[0][:2])
         minute = int(parse_time[0][2:])
         target_time = datetime.time(hour, minute)
         target_time_string = target_time.strftime("%H:%M")
 
+        # Create message text body based on who the reminder is for
         if text.split()[2] == "me":
             body = f"<@{user}>, I will send you a reminder at {target_time_string}."
         elif text.split()[2] == "everyone":
             body = f"<@{user}>, I will send everyone a reminder at {target_time_string}."
-        else:
-            return help_cmd(user, channel, client, bot_id)
 
         msg = {
             "ts": "",
@@ -154,9 +174,11 @@ def remind_cmd(user, channel, client, text, bot_id):
 
         send_msg(client, msg)
 
+        # Wait until the target time to send the reminder
         while datetime.datetime.now().time() < target_time:
             time.sleep(3)
 
+        # Create message text body based on who the reminder is for
         if text.split()[2] == "me":
             body = f"<@{user}>, this is your {target_time_string} reminder."
         elif text.split()[2] == "everyone":
@@ -199,6 +221,14 @@ def remind_cmd(user, channel, client, text, bot_id):
 
 # Command to get a response from OpenAI gpt-3.5-turbo
 def gpt_cmd(user, channel, client, text):
+    """
+    Get a ChatGPT response
+    :param user: user id
+    :param channel: channel id
+    :param client: slack client
+    :param text: text of the message
+    :return: none
+    """
 
     openai.api_key = os.environ['OPENAI_API_KEY']
 
